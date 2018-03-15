411c9f45fb6dd162426d75708f98f435aaa32a77
GoodNello <grislysquirrel@gmail.com>
2018-03-15 12:34:16 +0100

    0.2.4

--- imports/ui/Changelog.js
+++ imports/ui/Changelog.js
@@ -1,82 +1,74 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import Footer from './partials/Footer';
-import Header from './partials/Header';
+import Footer from "./partials/Footer";
+import Header from "./partials/Header";
 
 export default () => {
-    return (
-        <div className="boxed-view">
-            <div className="boxed-view__box">
-                <Header />
-                    <div className="boxed-view_box_list">
-                        <span>
-                            <h3>0.2</h3>
-                            <ul>
-                                <li>
-                                    <b>.3</b> Fixed pin input not working correctly
-                                </li>
-                                <li>
-                                    <b>.2</b> Fixed security issues
-                                </li>
-                                <li>
-                                    <b>.1</b> Preparation for Heroku deployment
-                                </li>
-                                <li>
-                                    <b>.0</b> Fixed styling issues
-                                </li>
-                                <li>
-                                    <b>.0</b> Added pages for development and how to use
-                                </li>
-                                <li>
-                                    <b>.0-alpha4</b> Styled components
-                                </li>
-                                <li>
-                                    <b>.0-alpha4</b> Added pin input boxes thanks to <a href="https://github.com/arunghosh/react-pin-input">react-pin-input</a>
-                                </li>
-                                <li>
-                                    <b>.0-alpha3</b> Refactored code
-                                </li>
-                                <li>
-                                    <b>.0-alpha3</b> Added controlled inputs
-                                </li>
-                                <li>
-                                    <b>.0-alpha3</b> Added changelog page
-                                </li>
-                                <li>
-                                    <b>.0-alpha3</b> Moved /code to /getcode
-                                </li>
-                                <li>
-                                    <b>.0-alpha2</b>Refactored code
-                                </li>
-                                <li>
-                                    <b>.0-alpha1</b>Refactored code
-                                </li>
-                                <li>
-                                    <b>.0-alpha1</b>Support for accessing links via form
-                                </li>
-                            </ul>
-                        </span>
-                        <span>
-                            <h3>0.1</h3>
-                            <ul>
-                                <li>
-                                    <b>.0</b> Removed non-working form for accessing links via form (exp. in next release)
-                                </li>
-                                <li>
-                                    <b>.0-alpha2</b> Input validation via <a href="https://github.com/aldeed/meteor-simple-schema">simpl-schema</a>
-                                </li>
-                                <li>
-                                    <b>.0-alpha2</b> Redirection via code in url
-                                </li>
-                                <li>
-                                    <b>0-alpha1</b> First developer release
-                                </li>
-                            </ul>
-                        </span>
-                    </div>
-                <Footer />
-            </div>
+  return (
+    <div className="boxed-view">
+      <div className="boxed-view__box">
+        <Header />
+        <div className="boxed-view_box_list">
+          <span>
+            <h3>0.2</h3>
+            <ul>
+              <li>
+                <b>.4</b> Added link to Amaro Team website
+              </li>
+              <li>
+                <b>.4</b> Removed older versions from changelog
+              </li>
+              <li>
+                <b>.3</b> Fixed pin input not working correctly
+              </li>
+              <li>
+                <b>.2</b> Fixed security issues
+              </li>
+              <li>
+                <b>.1</b> Preparation for Heroku deployment
+              </li>
+              <li>
+                <b>.0</b> Fixed styling issues
+              </li>
+              <li>
+                <b>.0</b> Added pages for development and how to use
+              </li>
+              <li>
+                <b>.0-alpha4</b> Styled components
+              </li>
+              <li>
+                <b>.0-alpha4</b> Added pin input boxes thanks to{" "}
+                <a href="https://github.com/arunghosh/react-pin-input">
+                  react-pin-input
+                </a>
+              </li>
+              <li>
+                <b>.0-alpha3</b> Refactored code
+              </li>
+              <li>
+                <b>.0-alpha3</b> Added controlled inputs
+              </li>
+              <li>
+                <b>.0-alpha3</b> Added changelog page
+              </li>
+              <li>
+                <b>.0-alpha3</b> Moved /code to /getcode
+              </li>
+              <li>
+                <b>.0-alpha2</b>Refactored code
+              </li>
+              <li>
+                <b>.0-alpha1</b>Refactored code
+              </li>
+              <li>
+                <b>.0-alpha1</b>Support for accessing links via form
+              </li>
+            </ul>
+          </span>
         </div>
-    );
-}
+        <Footer />
+      </div>
+    </div>
+  );
+};
--- imports/ui/partials/Footer.js
+++ imports/ui/partials/Footer.js
@@ -1,13 +1,33 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default () => {
-    return (
-        <div>
-            <p><b><Link to="/howtouse">How To Use</Link></b></p>
-            <p><b><Link to="/development">Development and Known Issues</Link></b></p>
-            <p><b><Link to="/changelog">0.2.3</Link> (Development Release)</b></p>
-            <p><b>Amaro Team ❤️☕</b></p>
-        </div>
-    );
-}
+  return (
+    <div className="footer">
+      <p>
+        <b>
+          <Link to="/howtouse">How To Use</Link>
+        </b>
+      </p>
+      <p>
+        <b>
+          <Link to="/development">Development and Known Issues</Link>
+        </b>
+      </p>
+      <p>
+        <b>
+          <Link to="/changelog" className="footer__nodec">
+            0.2.4 (Development Release)
+          </Link>
+        </b>
+      </p>
+      <p>
+        <b>
+          <a href="http://www.amaro.team/" className="footer__nodec">
+            Amaro Team ❤️☕
+          </a>
+        </b>
+      </p>
+    </div>
+  );
+};

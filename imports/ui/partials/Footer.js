0a92359797d042e46b4b22b7ed2932611a9d3c3d
GoodNello <grislysquirrel@gmail.com>
2018-03-23 17:24:56 +0100

    Revert "0.4.0-alpha1"
    This reverts commit 0927a056838b3b024423a220ed10ac52471191b0.
    Moved to experimental bootstrap branch.

--- imports/ui/partials/Footer.js
+++ imports/ui/partials/Footer.js
@@ -4,46 +4,28 @@ import { Link } from "react-router-dom";
 export default () => {
   return (
     <div className="footer">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="http://www.amaro.team/">
-          Amaro Team
-        </a>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNavFooter"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavFooter">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <Link className="nav-link" to="/about-api">
-                Extend via API
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about-link">
-                About
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/support">
-                Support
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/changelog">
-                0.4.0-alpha1
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <p>
+        <b>
+          <Link to="/howtouse">How To Use</Link>
+        </b>
+      </p>
+      <p>
+        <b>
+          <Link to="/about-api">Extend via API</Link>
+        </b>
+      </p>
+      <p>
+        <b>
+          <Link to="/changelog">0.3.0</Link> (Development Release)
+        </b>
+      </p>
+      <p>
+        <b>
+          <a href="http://www.amaro.team/" className="footer__nodec">
+            Amaro Team
+          </a>
+        </b>
+      </p>
     </div>
   );
 };
--- imports/ui/partials/Header.js
+++ imports/ui/partials/Header.js
@@ -1,32 +1,10 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-import User from "./header/User";
-
-export default class Header extends React.Component {
-  render() {
+export default () => {
     return (
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top navbar-background">
-          <Link className="navbar-brand" to="/">
-            <span className="white-text">Amaro Link</span>
-          </Link>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <img className="user-icon" src="/images/icons/user.svg" />
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <User isUser={this.props.isUser} />
-          </div>
-        </nav>
-      </header>
+        <div className="header">
+            <h1><Link to="/">Amaro Link</Link></h1>
+        </div>
     );
-  }
 }
